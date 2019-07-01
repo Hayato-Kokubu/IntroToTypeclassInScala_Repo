8246097173b6740e7a9cab102556779b6ecc25e8
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-07-01 20:20:29 +0900

    型引数を持つ型のインスタンス宣言では、implicit def を用いる

--- src/main/scala/Comparators.scala
+++ src/main/scala/Comparators.scala
@@ -1,4 +1,10 @@
 object Comparators {
   implicit object IntComparator extends Comparator[Int] {
     def compare(a: Int, b: Int): Int = a - b }
+
+  implicit def setSizeComparator[T]: Comparator[Set[T]] ={
+    new Comparator[Set[T]] {
+      def compare(a: Set[T], b: Set[T]): Int = a.size - b.size
+    }
+  }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,8 +3,15 @@ import Comparators._
 object Main extends App{
 
   val sortedList = sort(List(6, 1, 4, 7, 5, 2, 9, 3, 8))
-
   println(sortedList)
+
+
+  val sortedSetsList1 = sort(List(Set(1,2,3), Set.empty[Int], Set(2,4,6,8), Set(2,3,5,8,9)))
+  // インスタンス宣言のため、Set.empty のInt は必要
+  println(sortedSetsList1)
+
+  val sortedSetsList2 = sort(List(Set("one", "two"), Set("1", "2", "3")))
+  println(sortedSetsList2)
 
   def sort[T](list: List[T])(implicit comparator: Comparator[T]): List[T] = {
     list match {
